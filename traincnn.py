5dff2dfb89c3d85c544392a4eef9f4876811e531
thanhxuan2k5 <xuanng205@gmail.com>
2025-11-05 09:02:39 +0700

    first commit

--- traincnn.py
+++ traincnn.py
@@ -176,4 +176,3 @@ if __name__ == '__main__':
             torch.save(checkpoint, "{}/best.pt".format(args.trained_models))
             best_acc = accuracy
         # print(classification_report(all_labels, all_predictions))
-git remote add origin
